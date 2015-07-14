72352128f587414d3e22df2cc884dae295150866
kradical <schultzk@uvic.ca>
2015-07-14 11:47:17 -0700

    input

---
+++ SSS.py
@@ -0,0 +1,6 @@
+def main():
+        prompt = """Input a string representation of a sudoku puzzle!\n * is a space and digits are numbers\nexample: *********************************************************************************\nis a fully blank puzzle while\nexample: 1**3****************************************************************************9\nhas a 1 in the top corner, a 3 in the fourth column of the first row and a 9 in the bottom right corner\ninput: """
+	puzzle = input(prompt)
+
+if __name__ == "__main__":
+	main()
