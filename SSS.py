435407418320ceb1d125c42762f9bbf1dc6b0388
kradical <schultzk@uvic.ca>
2015-07-14 13:24:45 -0700

    commiterino

--- SSS.py
+++ SSS.py
@@ -29,13 +29,12 @@ def xijkString(puzzle):
 			info = str(column) + str(row) + char + ','
 			result += info
 	return result[0:-1]
-    
 
-
-def printPuzzle(puzzle):
+def printPuzzle(index, puzzle):
     """takes a string representation of a sudoku puzzle
     and prints it out in pretty format!
     """
+    print "Puzzle " + str(index+1)
     for x in xrange(9):
         if x % 3 == 0 and x != 0:
             print"--"*11
@@ -44,39 +43,27 @@ def printPuzzle(puzzle):
                 print"|",
             print " " if puzzle[9*x + y] == '*' else puzzle[9*x + y],
         print 
+    print 
+    return puzzle.rstrip()
+
+def isValid(puzzle):
+    """ takes string representation of a puzzle and
+    returns if it is valid or not
+    """
+    if(len(puzzle) != 81):
+        return False
+    valid_chars = {'0','1','2','3','4','5','6','7','8','9','*'}
+    puzzleset = set(puzzle)
+    return puzzleset.issubset(valid_chars)
 
 def main():
-    prompt = "Input a string representation of a sudoku puzzle!\n\
-* is a space and digits are numbers\n\
-example: *********************************************************************************\n\
-is a fully blank puzzle while\n\
-example: 1**3****************************************************************************9\n\
-has a 1 in the top corner, a 3 in the fourth column of the first row and a 9 in the bottom right corner\n\
-input: "
-    #[printPuzzle(x) for x in open(sys.argv[1],'r')]
-    puzzle = raw_input(prompt)
-    valid_chars = ['0','1','2','3','4','5','6','7','8','9','*']
-
-    if(len(puzzle) != 81):
-    	print "Incorrect Puzzle Length" + len(puzzle)
-    	exit(1)
-
-    for i in range(0,len(puzzle)):
-    	char = puzzle[i]
-    	if(char not in valid_chars):
-    		print "Incorrect Character " + str(char)
-    		exit(1)
-
-
-    printPuzzle(puzzle)
-
-    xijkList = xijkString(puzzle)
-
+    puzzlelist = [printPuzzle(ndx, x) for ndx,x in enumerate(open(sys.argv[1], 'r')) if isValid(x.rstrip())]
+    xijkList = xijkString(puzzlelist[3])
     xijk = xijkList.split(',')
+    print xijk
     for value in xijk:
     	temp = uniqueInteger(value)
-
-
+        print temp,
 
 
 if __name__ == "__main__":
