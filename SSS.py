11beea6523a5a1e174db6ed949e7d5972f4a6a4a
kradical <schultzk@uvic.ca>
2015-07-14 12:28:27 -0700

    samples

--- SSS.py
+++ SSS.py
@@ -1,3 +1,5 @@
+import sys
+
 def printPuzzle(puzzle):
     """takes a string representation of a sudoku puzzle
     and prints it out in pretty format!
@@ -19,6 +21,7 @@ is a fully blank puzzle while\n\
 example: 1**3****************************************************************************9\n\
 has a 1 in the top corner, a 3 in the fourth column of the first row and a 9 in the bottom right corner\n\
 input: "
+    [printPuzzle(x) for x in open(sys.argv[1],'r')]
     puzzle = raw_input(prompt)
     valid_chars = ['0','1','2','3','4','5','6','7','8','9','*']
 
