c8fcf6fbbc24e66be85e1852e0bbafdc611c1d74
Dustin Bolink <bolinkd@uvic.ca>
2015-07-14 12:47:15 -0700

    Created the Xijk string to be parsed

--- SSS.py
+++ SSS.py
@@ -1,5 +1,22 @@
 import sys
 
+def xijkString(puzzle):
+	"""
+	Takes a puzzle string and puts it
+	into Xijk format
+	"""
+	result = ""
+	for i in range(0,len(puzzle)):
+		char = puzzle[i]
+		column = i%9+1
+		row = i/9 + 1
+		if(char != '*'):
+			info = str(column) + str(row) + char + ','
+			result += info
+	return result[0:-1]
+    
+
+
 def printPuzzle(puzzle):
     """takes a string representation of a sudoku puzzle
     and prints it out in pretty format!
@@ -21,7 +38,7 @@ is a fully blank puzzle while\n\
 example: 1**3****************************************************************************9\n\
 has a 1 in the top corner, a 3 in the fourth column of the first row and a 9 in the bottom right corner\n\
 input: "
-    [printPuzzle(x) for x in open(sys.argv[1],'r')]
+    #[printPuzzle(x) for x in open(sys.argv[1],'r')]
     puzzle = raw_input(prompt)
     valid_chars = ['0','1','2','3','4','5','6','7','8','9','*']
 
@@ -37,6 +54,9 @@ input: "
 
 
     printPuzzle(puzzle)
+
+    xijk = xijkString(puzzle)
+    print xijk
 
 
 
