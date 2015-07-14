0afa9bc876ca9150c23c7a6314ca1b9396836dba
kradical <schultzk@uvic.ca>
2015-07-14 12:19:39 -0700

    pretty printing

--- SSS.py
+++ SSS.py
@@ -1,8 +1,15 @@
 def printPuzzle(puzzle):
-	"""takes a string representation of a sudoku puzzle
-	and prints it out 
-	"""
-	pass
+    """takes a string representation of a sudoku puzzle
+    and prints it out in pretty format!
+    """
+    for x in xrange(9):
+        if x % 3 == 0 and x != 0:
+            print"--"*11
+        for y in xrange(9):
+            if y % 3 == 0 and y != 0:
+                print"|",
+            print " " if puzzle[9*x + y] == '*' else puzzle[9*x + y],
+        print 
 
 def main():
     prompt = "Input a string representation of a sudoku puzzle!\n\
@@ -12,8 +19,9 @@ is a fully blank puzzle while\n\
 example: 1**3****************************************************************************9\n\
 has a 1 in the top corner, a 3 in the fourth column of the first row and a 9 in the bottom right corner\n\
 input: "
-    puzzle = input(prompt)
+    puzzle = raw_input(prompt)
+    printPuzzle(puzzle)
 
 
 if __name__ == "__main__":
-	main()
+    main()
