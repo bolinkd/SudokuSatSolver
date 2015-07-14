c6744194a21d369f1b69afe9489e3d788d9682e9
kradical <schultzk@uvic.ca>
2015-07-14 11:58:30 -0700

    multiline prompt

--- SSS.py
+++ SSS.py
@@ -1,6 +1,19 @@
+def printPuzzle(puzzle):
+	"""takes a string representation of a sudoku puzzle
+	and prints it out 
+	"""
+	pass
+
 def main():
-        prompt = """Input a string representation of a sudoku puzzle!\n * is a space and digits are numbers\nexample: *********************************************************************************\nis a fully blank puzzle while\nexample: 1**3****************************************************************************9\nhas a 1 in the top corner, a 3 in the fourth column of the first row and a 9 in the bottom right corner\ninput: """
-	puzzle = input(prompt)
+    prompt = "Input a string representation of a sudoku puzzle!\n\
+* is a space and digits are numbers\n\
+example: *********************************************************************************\n\
+is a fully blank puzzle while\n\
+example: 1**3****************************************************************************9\n\
+has a 1 in the top corner, a 3 in the fourth column of the first row and a 9 in the bottom right corner\n\
+input: "
+    puzzle = input(prompt)
+
 
 if __name__ == "__main__":
 	main()
