9f3c69e066b36798fbb36673aedab593f20e7e59
kradical <schultzk@uvic.ca>
2015-07-15 16:12:03 -0700

    refactored code! same loops just made a convert function instead of converting int->str->int->str. Outputs exactly same as before :)

--- SSS.py
+++ SSS.py
@@ -1,44 +1,29 @@
 import sys
 
 
-def RuleToBooleans(rules):
-	result = rules
-	for ndx, item in enumerate(rules):
-		result[ndx] = 81*(int(item[0])-1) + 9*(int(item[1])-1) + (int(item[2])-1) + 1 
-	return result
+def convert10to9(hundreds, tens, ones):
+	# converts a base 10 number to base 9
+	return str(81*(hundreds-1) + 9 * (tens-1) + ones)
 
 def HeaderInfo(output, num_clauses):
 	output.write("p cnf 729 "+str(num_clauses)+"\n")
 
 def EveryCellOneNumber(output):
-	# Every cell must contain at least one number!
     output.write(''.join((str(ndx) + " 0\n" if ndx % 9 == 0 else str(ndx) + " " for ndx in xrange(1, 730))))
 
 def EveryNumberOnceInRow(output):
-	for i in range(1,10):
+    for i in range(1,10):
 		for k in range(1,10):
 			for j in range(1,9):
 				for l in range(j+1, 10):
-					rule = []
-					rule.append(str(i)+str(j)+str(k))
-					rule.append(str(i)+str(l)+str(k))
-					printable = ""
-					for boolean in RuleToBooleans(rule):
-						printable += "-" + str(boolean) + " "
-					output.write(printable + "0\n")
+					output.write("-"+convert10to9(i, j, k)+" -"+convert10to9(i, l, k)+" 0\n")
 
 def EveryNumberOnceInColumn(output):
 	for j in range(1,10):
 		for k in range(1,10):
 			for i in range(1,9):
 				for l in range(i+1, 10):
-					rule = []
-					rule.append(str(i)+str(j)+str(k))
-					rule.append(str(l)+str(j)+str(k))
-					printable = ""
-					for boolean in RuleToBooleans(rule):
-						printable += "-" + str(boolean) + " "
-					output.write(printable + "0\n")
+					output.write("-"+convert10to9(i, j, k)+" -"+convert10to9(l, j, k)+" 0\n")
 
 def EveryNumberOnceInBox(output):
 	for k in range(1,10):
@@ -47,13 +32,8 @@ def EveryNumberOnceInBox(output):
 				for u in range(1,4):
 					for v in range(1,3):
 						for w in range(v+1,3):
-							rule = []
-							rule.append(str(3*a+u)+str(3*b+v)+str(k))
-							rule.append(str(3*a+u)+str(3*b+w)+str(k))
-							printable = ""
-							for boolean in RuleToBooleans(rule):
-								printable += "-" + str(boolean) + " "
-							output.write(printable + "0\n")
+							output.write("-"+convert10to9(3*a+u, 3*b+v, k)+" -"+convert10to9(3*a+u, 3*b+w, k)+" 0\n")
+
 	for k in range(1,10):
 		for a in range(0,3):
 			for b in range(0,3):
@@ -61,33 +41,11 @@ def EveryNumberOnceInBox(output):
 					for v in range(1,4):
 						for w in range(u+1,4):
 							for t in range(1,4):
-								rule = []
-								rule.append(str(3*a+u)+str(3*b+v)+str(k))
-								rule.append(str(3*a+w)+str(3*b+t)+str(k))
-								printable = ""
-								for boolean in RuleToBooleans(rule):
-									printable += "-" + str(boolean) + " "
-								output.write(printable + "0\n")
-
-
-							
+								output.write("-"+convert10to9(3*a+u, 3*b+v, k)+" -"+convert10to9(3*a+w, 3*b+t, k)+" 0\n")							
 
 def PuzzleToBooleans(puzzle):
 	# Converts a puzzle string to a list of variables that must be True for the SAT solver
-	return [81*(ndx%9) + 9*(ndx/9) + int(char) for ndx, char in enumerate(puzzle) if char.isdigit()]
-
-def PrettifyPuzzle(puzzle):
-    # takes a string representation of a sudoku puzzle and prints it out in pretty format!
-    pretty_puzzle = ""
-    for x in xrange(9):
-        if x % 3 == 0 and x != 0:
-            pretty_puzzle += "--"*11 + "\n"
-        for y in xrange(9):
-            if y % 3 == 0 and y != 0:
-                pretty_puzzle += "| "
-            pretty_puzzle += "  " if puzzle[9*x + y] == '*' else puzzle[9*x + y]+" "
-        pretty_puzzle += "\n"
-    return pretty_puzzle
+	return [convert10to9((ndx%9+1), (ndx/9+1), int(char)) for ndx, char in enumerate(puzzle) if char.isdigit()]
 
 def IsValid(puzzle):
     # takes string representation of a puzzle and returns if it is valid or not
@@ -108,8 +66,7 @@ def main():
             EveryNumberOnceInRow(fp)
             EveryNumberOnceInColumn(fp)
             EveryNumberOnceInBox(fp)
-            fp.write(" 0\n".join((str(expr) for expr in puzzle)) + " 0\n")
-
+            fp.write(" 0\n".join(puzzle) + " 0\n")
 
 if __name__ == "__main__":
     main()
